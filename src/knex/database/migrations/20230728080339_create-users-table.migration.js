b63ae0b5efec8eb72a95cab54b0522e214f60a0e
Grish Poghosyan <grisha.poghosyan.dev@gmail.com>
2023-07-28 12:51:46 +0400

    feat: migration pipeline impl

---
+++ src/knex/database/migrations/20230728080339_create-users-table.migration.js
@@ -0,0 +1,21 @@
+import { TABLE_USERS } from "../../table-names.js";
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable(TABLE_USERS, (table) => {
+    table.increments("id");
+    table.string("username").unique().notNullable();
+    table.string("password").notNullable();
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable(TABLE_USERS);
+}
--- src/knex/knexfile.js
+++ src/knex/knexfile.js
@@ -1,16 +1,21 @@
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: "../../.env" });
+
 export default {
   client: process.env.DB_CLIENT,
   connection: {
-    database: process.env.DB_DB,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
   },
   pool: {
-    min: process.env.DB_CONN_POLL_MIN,
-    max: process.env.DB_CONN_POLL_MAX,
+    min: +process.env.DB_CONN_POLL_MIN,
+    max: +process.env.DB_CONN_POLL_MAX,
   },
+  propagateCreateError: false,
   acquireConnectionTimeout: process.env.DB_CONN_TIMEOUT,
   migrations: {
     directory: `./database/migrations`,
