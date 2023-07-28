679da644791668f1e4fb78d2fdaa25e53da26f9a
Grish Poghosyan <grisha.poghosyan.dev@gmail.com>
2023-07-28 11:08:27 +0400

    chore: minor change

--- src/errors/customError.error.js
+++ src/errors/customError.error.js
@@ -1,10 +1,8 @@
 export class CustomError extends Error {
   constructor(params) {
     const { message, status } = params;
+    super(message);
 
-    super();
-
-    this.message = message;
     this.status = status;
   }
 }
