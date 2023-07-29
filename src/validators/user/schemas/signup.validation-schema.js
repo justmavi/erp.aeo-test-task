3d1e0e945331e5b23bd28b19e92f87d155d8fa58
Grish Poghosyan <grisha.poghosyan.dev@gmail.com>
2023-07-29 11:08:31 +0400

    feat: allow phone numbers in login

--- src/validators/user/schemas/signup.validation-schema.js
+++ src/validators/user/schemas/signup.validation-schema.js
@@ -3,7 +3,10 @@ import { USER_PASSWORD_MIN_LENGTH } from "../../../constants";
 
 export const signupValidationSchema = Joi.object({
   body: Joi.object({
-    username: Joi.string().email({ tlds: { allow: false } }),
+    username: Joi.alternatives().try(
+      Joi.string().email({ tlds: { allow: false } }),
+      Joi.string().pattern(/^\+?\d{8,15}$/)
+    ),
     password: Joi.string().min(USER_PASSWORD_MIN_LENGTH),
   }),
 }).unknown(true);
